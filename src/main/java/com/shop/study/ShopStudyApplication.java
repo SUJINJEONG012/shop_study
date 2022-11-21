641009117721d3614c55bf877376f9819d735064
SUJINJEONG012 <peekaboo32@naver.com>
2022-11-21 12:51:51 +0900

    쇼핑몰프로젝트구현하기

---
+++ src/main/java/com/shop/study/ShopStudyApplication.java
@@ -0,0 +1,13 @@
+package com.shop.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopStudyApplication.class, args);
+	}
+
+}
