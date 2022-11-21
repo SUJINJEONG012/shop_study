e1283e8b26dba5786e7c4d2f35497c209b4c4800
SUJINJEONG012 <peekaboo32@naver.com>
2022-11-22 08:45:26 +0900

    쇼핑몰프로퍼티설정

--- src/main/java/com/shop/study/ShopStudyApplication.java
+++ src/main/java/com/shop/study/ShopStudyApplication.java
@@ -2,12 +2,20 @@ package com.shop.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ShopStudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopStudyApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String HelloWorld() {
+		return "hello world!";
 	}
 
 }
